62f4b5bfaad07f2427a898fdb73d93c3d7101d40
Marlon-Orrego <marlonorrego11@gmail.com>
2023-02-07 17:15:17 -0500

    Ejercicio Clase 1

---
+++ 05-Centro Asistencial.dart
@@ -0,0 +1,61 @@
+import 'dart:io';
+
+void main(List<String> args) {
+  //Ingresando Cantidad de lisiados
+  print("Ingrese la cantidad de lisiados de la semana: ");
+  int lisiados = int.parse(stdin.readLineSync().toString());
+  
+  //Declarando Variables
+  var hombreSoltero = 0;
+  var hombresCasados = 0;
+  var hombres = 0;
+  var edadPromedio = 0;
+  var personasSolteras = 0;
+  var mujerSoltera = 0;
+
+  for (int i = 0; i < lisiados; i++) {
+    var sexo = 0;
+    var edad = 0;
+    var estCivil = 0;
+    //Ingresando Sexo
+    print("Ingrese el sexo del Lisiado ${i + 1}: 1. Hombre  2. Mujer");
+    sexo = int.parse(stdin.readLineSync().toString());
+    if (sexo == 1) {
+      hombres++;
+    }
+    //Ingresando Estado Civil
+    print(
+        "Ingrese el estado civil del Lisiado # ${i + 1}: 1. Soltero  2. Casado 3. Viudo");
+
+    estCivil = int.parse(stdin.readLineSync().toString());
+
+    if (estCivil == 1 && sexo == 2) {
+      mujerSoltera++;
+    }
+    if (estCivil == 1) {
+      personasSolteras++;
+    }
+
+    if (estCivil == 1 && sexo == 1) {
+      hombreSoltero++;
+    }
+
+    //Edad
+    print("ingrese la edad del Lisiado ${i + 1}: ");
+    edad = int.parse(stdin.readLineSync().toString());
+
+    if (sexo == 1 && estCivil == 2) {
+      edadPromedio += edad;
+      hombresCasados++;
+    }
+  }
+  var calcProm = ((hombreSoltero * 100) / hombres);
+  print("El porcentaje de hombres solteros es de: $calcProm");
+
+  var calcEdad = edadPromedio / hombresCasados;
+  print("La edad promedio de los hombres casados es de: $calcEdad");
+
+  var calcMujeresSolteras = ((mujerSoltera * 100) / personasSolteras);
+  print(
+      "El porcebtaje de mujeres solteras en relacion a las personas solteras es de: $calcMujeresSolteras");
+}
